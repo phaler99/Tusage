ffbd0cc0505efc11bd47a6a857258d2770a6ce97
phaler99 <phaler99@gmail.com>
2025-07-06 21:43:19 +0300

    appsss

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Tusage_FILE C:/projects/Tusage/build/Tusage.exe)
+set(__QT_DEPLOY_TARGET_Tusage_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Tusage_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
---
+++ build/CMakeFiles/Tusage.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tusage.dir/main.cpp.obj"
+  "CMakeFiles/Tusage.dir/main.cpp.obj.d"
+  "Tusage.exe"
+  "Tusage.exe.manifest"
+  "Tusage.pdb"
+  "libTusage.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tusage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
