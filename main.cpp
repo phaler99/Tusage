0ba55f763352822f882a6088b1d4e9f047621bf9
phaler99 <phaler99@gmail.com>
2025-07-06 21:49:33 +0300

    123

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QMainWindow window;
     window.setWindowTitle("Tusage");
-    window.resize(400, 300);
+    window.resize(800, 600);
     window.show();
     return app.exec();
 }
